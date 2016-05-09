58bb55c4748da67dad11e371a73bf182826f556c
Ryan <ryan@Ryans-MacBook-Pro.local>
2016-05-09 12:10:58 -0400

    added backend code

---
+++ Backend/connectionManager.php
@@ -0,0 +1,40 @@
+<?php
+    class ConnectionManager {
+        public $host = "localhost";
+        public $user = "umdandro";
+        public $password = "cmscpass1";
+        public $database = "umdandro_app";
+        public $db_connection;
+
+        public function __construct() {
+            $this->db_connection = new mysqli($this->host, $this->user, $this->password, $this->database);
+        }
+
+
+        function disconnect() {
+            $this->db_connection->close();
+        }
+
+        function insert($sql) {
+            $result = $this->db_connection->query($sql);
+            if (!$result) {
+                return false;
+            }
+            else {
+                return true;
+            }
+        }
+
+        function get($sql) {
+            $result = $this->db_connection->query($sql);
+            if (!$result) {
+                return false;
+            }
+            else {
+                return $result;
+            }
+        }
+
+    }
+
+?>
